91562e5135605a009672470f8eda937beeca02e7
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-03-25 16:06:24 -0300

    [LEARNING - TESTING]: Test a click on a Checkbox

---
+++ apps/playground/src/app/domains/testing/screens/testing.tsx
@@ -0,0 +1,11 @@
+import { Checkbox } from '../components/checkbox';
+import { Toggle } from '../components/toggle';
+
+export const Testing = () => {
+  return (
+    <div>
+      <Toggle />
+      <Checkbox label="Select me!" />
+    </div>
+  );
+};
