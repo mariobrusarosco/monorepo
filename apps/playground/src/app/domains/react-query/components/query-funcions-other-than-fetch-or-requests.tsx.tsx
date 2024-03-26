6b1c68d10ee3fec168a459bddda27a2bf887c95b
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-03-26 13:34:20 -0300

    [LEARNING - REACT QUERY]: Dealing with data other than fetch or network requests

---
+++ apps/playground/src/app/domains/react-query/components/query-funcions-other-than-fetch-or-requests.tsx.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
+
+async function getLocation() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+export const QueryFuncionsOtherThanFetchOrRequests = () => {
+  const locationQuery = useQuery({
+    queryKey: ['location'],
+    queryFn: getLocation,
+  });
+
+  if (locationQuery.isLoading) {
+    return <p>Calculating location...</p>;
+  }
+
+  if (locationQuery.error) {
+    return <p>Error: {locationQuery.error.message}</p>;
+  }
+
+  return (
+    <div>
+      <h2>Dealing with data other than fetch or network requests </h2>
+      <p>
+        Your location is:
+        {locationQuery.data.coords.latitude},
+        {locationQuery.data.coords.longitude}
+      </p>
+    </div>
+  );
+};
