da0af8c20770f7e71236c21039e0b9c06c5e056f
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-03-26 11:44:49 -0300

    [LEARNING - REACT QUERY]: setup

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,40 +1,44 @@
 import './app.css';
 import { ReactQuery } from './domains/react-query/screens/reac-query';
 import { Testing } from './domains/testing/screens/testing';
-
-import NxWelcome from './nx-welcome';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Route, Routes, Link } from 'react-router-dom';
+
+const client = new QueryClient();
 
 export function App() {
   return (
     <div>
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/testing">Testing</Link>
-          </li>
-          <li>
-            <Link to="/react-query">React Query</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <Link to="/testing">Click here for Testing.</Link>
-            </div>
-          }
-        />
-        <Route path="/testing" element={<Testing />} />
-        <Route path="/react-query" element={<ReactQuery />} />
-      </Routes>
-      {/* END: routes */}
+      <QueryClientProvider client={client}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <div role="navigation">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/testing">Testing</Link>
+            </li>
+            <li>
+              <Link to="/react-query">React Query</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Link to="/testing">Click here for Testing.</Link>
+              </div>
+            }
+          />
+          <Route path="/testing" element={<Testing />} />
+          <Route path="/react-query" element={<ReactQuery />} />
+        </Routes>
+        {/* END: routes */}
+      </QueryClientProvider>
     </div>
   );
 }
