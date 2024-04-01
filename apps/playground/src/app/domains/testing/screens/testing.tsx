4f27eb9df60565bd7665eb7b147655ba8d5c31c2
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-04-01 17:28:52 -0300

    [LEARNING = TESTING] - Test Componen that has performed a mouseEnter and mouseLeave

--- apps/playground/src/app/domains/testing/screens/testing.tsx
+++ apps/playground/src/app/domains/testing/screens/testing.tsx
@@ -1,11 +1,13 @@
 import { Checkbox } from '../components/checkbox';
 import { Toggle } from '../components/toggle';
+import { Tooltip } from '../components/tooltip';
 
 export const Testing = () => {
   return (
     <div>
       <Toggle />
       <Checkbox label="Select me!" />
+      <Tooltip />
     </div>
   );
 };
