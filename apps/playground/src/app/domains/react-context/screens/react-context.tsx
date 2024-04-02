bcc37c133952b612b3f5ab50fada707cc474988c
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-04-02 17:32:52 -0300

    [NO SCOPE]: updating context related code

--- apps/playground/src/app/domains/react-context/screens/react-context.tsx
+++ apps/playground/src/app/domains/react-context/screens/react-context.tsx
@@ -1,5 +1,5 @@
 import { UsingViaUseContext } from '../components/using-via-custom-hook-and-use-reducer';
-import { UsingViaCustomHookAndUseReducer } from '../components/using-via-use-context';
+import { UsingViaCustomHookAndUseReducer } from '../components/using-via-custom-hook-and-use-reducer';
 import { NotificationProvider } from '../context/notification-context';
 
 export const ReactContextScreen = () => {
