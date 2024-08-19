364708355d1b7c4bfce18eaf75666b56927ec29e
mario <mariobrusarosco@hotmail.com>
2024-08-19 13:24:08 -0300

    start: 0.1.0

--- apps/monorepo/src/app/app.tsx
+++ apps/monorepo/src/app/app.tsx
@@ -1,14 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="monorepo" />
-    </div>
-  );
+  return <div>Start</div>;
 }
 
 export default App;
